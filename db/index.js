5e1cf20b1a4197955e30565bf09960a211da5be3
zhangsong <zhangsong@beeboxes.com>
2019-01-25 17:16:42 +0800

    add npms

---
+++ db/index.js
@@ -0,0 +1,32 @@
+var mysql = require('mysql');
+let config = {
+  host: 'localhost',
+  user: 'root',
+  password: 'Song1314!',
+  database: 'mpbase',
+  port: 3306,
+  multipleStatements: true//允许多条sql同时执行
+};
+let pool = mysql.createPool(config);
+let query = (sql, values) => {
+
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        reject(err)
+      } else {
+        connection.query(sql, values, (err, rows) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(rows)
+          }
+          connection.end()
+        })
+      }
+    })
+  })
+};
+module.exports = {
+  query
+}
